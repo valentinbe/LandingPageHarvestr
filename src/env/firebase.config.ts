604be0f78e18c6fa2f811f4df0dc8d1f0aa80b35
Valentin <valentinraspi@gmail.com>
2017-05-03 00:12:55 +0200

    links fix

--- src/env/firebase.config.ts
+++ src/env/firebase.config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: '"'AIzaSyD0C3mhM_xjguTHJBptkUD6pzquW4v-UlI',
+  apiKey: 'AIzaSyD0C3mhM_xjguTHJBptkUD6pzquW4v-UlI',
   authDomain: 'harvestr-c83a9.firebaseapp.com',
   databaseURL: 'https://harvestr-c83a9.firebaseio.com',
   storageBucket: 'harvestr-c83a9.appspot.com',

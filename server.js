26217f4965b06ac567252aa7a43ebd15f6c52a3c
Valentin <valentinraspi@gmail.com>
2017-05-02 15:46:12 +0200

    testtttt

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -12,6 +13,6 @@ app.listen(process.env.PORT || 8080);
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });

682cce0cc70a393e265ca31bd5dce9c70b15e774
Valentin <valentinraspi@gmail.com>
2017-05-03 18:31:38 +0200

    responsive iphone

--- server.js
+++ server.js
@@ -24,18 +24,33 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '8080';
-app.set('port', port);
+//const port = process.env.PORT || '8080';
+//app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on port:${port}`));
+//server.listen(port, () => console.log(`API running on port:${port}`));
+
+
+// Run the app by serving the static files
+// in the dist directory
+//app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default
+// Heroku port
+//app.listen(process.env.PORT || 8080);
+

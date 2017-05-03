8396c597275d0a21d25e120d8f1803cc92bd6708
Valentin <valentinraspi@gmail.com>
2017-05-03 18:48:55 +0200

    responsive iphone

--- server.js
+++ server.js
@@ -5,6 +5,3 @@ const path = require('path');
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 3000);

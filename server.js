1db56d9d6ba462cdf34288c5f2cfb9d536f8dc27
Valentin <valentinraspi@gmail.com>
2017-05-03 18:44:22 +0200

    responsive iphone

--- server.js
+++ server.js
@@ -8,10 +8,3 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 3000);
-
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});

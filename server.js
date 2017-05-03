cb781c3e07bba64dec8679100f7a856fd5da1fde
Valentin <valentinraspi@gmail.com>
2017-05-03 18:34:33 +0200

    responsive iphone

--- server.js
+++ server.js
@@ -1,56 +1,17 @@
-// Get dependencies
 const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-
-// Get our API routes
-const api = require('./server/routes/api');
-
 const app = express();
-
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
-
-/**
- * Get port from environment and store in Express.
- */
-//const port = process.env.PORT || '8080';
-//app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-//const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-//server.listen(port, () => console.log(`API running on port:${port}`));
-
-
-// Run the app by serving the static files
-// in the dist directory
-//app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default
-// Heroku port
-//app.listen(process.env.PORT || 8080);
-
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
